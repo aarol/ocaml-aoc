6e3bb82ed17d9a8539deda8f6b954ea7cf2f368b
aarol <aarol@iki.fi>
2024-12-10 14:03:43 +0200

    day10

--- day09.ml
+++ day09.ml
@@ -2,61 +2,79 @@ open Core
 
 let digit_from_char c = int_of_char c - 48
 
-type block_value = Free | Sized of int
-type block = { len : int; value : block_value }
+(* type block_value = Free | Sized of int
+   type block = { len : int; value : block_value } *)
+(*
+   let to_block_list line =
+     Array.mapi line ~f:(fun i c ->
+         if i % 2 = 0 then { len = digit_from_char c; value = Sized (i / 2) }
+         else { len = digit_from_char c; value = Free }) *)
 
-let to_block_list line =
-  Array.map line ~f:(fun i c ->
-      if i % 2 = 0 then { len = digit_from_char c; value = Sized (i / 2) }
-      else { len = digit_from_char c; value = Free })
-
-let to_char_list line =
+let to_str_list line =
   Array.concat_mapi line ~f:(fun i c ->
       if i % 2 = 0 then
-        Array.init (digit_from_char c) ~f:(fun i -> string_of_int (i / 2))
+        Array.init (digit_from_char c) ~f:(fun _ -> string_of_int (i / 2))
       else Array.init (digit_from_char c) ~f:(fun _ -> "."))
 
 let part1 line =
-  let list = to_block_list line in
+  let arr = to_str_list line in
 
-  let rec aux i last_i =
-    match 
-    | [] -> 0
-    | x :: xs -> (
-        if (Char.equal x '.') then 
-          (i * digit_from_char x) + aux (i + 1) xs
-        else
-          
-            
+  let rec aux i last_i arr =
+    if last_i < i then 0
+    else
+      let x = arr.(i) in
+      if not @@ String.equal x "." then
+        (int_of_string x * i) + aux (i + 1) last_i arr
+      else
+        (* "." at i *)
+        let last = arr.(last_i) in
+        if String.equal last "." then aux i (last_i - 1) arr
+        else (int_of_string last * i) + aux (i + 1) (last_i - 1) arr
   in
 
-  aux 0 list |> Utils.print_int;
+  aux 0 (Array.length arr - 1) arr |> Utils.print_int;
 
   ()
 
-let part2 line =
-  let list = to_block_list line in
+(* let part2 line =
+  let arr = to_str_list line in
 
-  let rec aux i = function
-    | [] -> 0
-    | x :: xs -> (
-        Utils.print_int i;
-        if not (String.equal x ".") then (i * int_of_string x) + aux (i + 1) xs
-        else
-          match List.last xs with
-          | Some last ->
-              if String.equal last "." then aux i (List.drop_last_exn (x :: xs))
-              else
-                (i * int_of_string last) + aux (i + 1) (List.drop_last_exn xs)
-          | None -> 0)
+  let rec checksum_of_range a b =
+    if a = b then 0
+    else (int_of_string arr.(a) * a) + checksum_of_range (a + 1) b
   in
 
-  aux 0 list |> Utils.print_int;
+  let rec first_index_of_file start =
+    if start < 1 || (not @@ String.equal arr.(start - 1) ".") then start
+    else first_index_of_file (start - 1)
+  in
+  
+  let find_free length = 
+    for i = 0 to Array.length arr - length do
+      if String.equal arr.(i) "." 
+      
+      done
+      in
 
-  () ()
+  let rec aux right_i arr =
+    if right_i = 0 then 0
+    else
+      let x = arr.(right_i) in
+
+      if not @@ String.equal x "." then
+        let left_i = first_index_of_file right_i in
+        let length = right_i - left_i in
+
+        
+      else aux (right_i - 1) arr
+  in
+
+  aux (Array.length arr - 1) arr |> Utils.print_int;
+
+  () *)
 
 let () =
-  let lines = Utils.read_string "input/day09.txt" |> String.to_list in
+  let lines = Utils.read_string "input/day09.txt" |> String.to_array in
 
   part1 lines;
-  part2 lines
+  (* part2 lines *)
---
+++ day10.ml
@@ -0,0 +1,56 @@
+open Core
+
+let neighbours (x, y) = [ (x + 1, y); (x, y + 1); (x - 1, y); (x, y - 1) ]
+
+let next_target = function
+  | '0' -> '1'
+  | '1' -> '2'
+  | '2' -> '3'
+  | '3' -> '4'
+  | '4' -> '5'
+  | '5' -> '6'
+  | '6' -> '7'
+  | '7' -> '8'
+  | '8' -> '9'
+  | d -> failwithf "invalid target: %c" d ()
+
+let grid =
+  Utils.read_lines "input/day10.txt"
+  |> Array.of_list
+  |> Array.map ~f:String.to_array
+
+let grid_w = Array.length grid.(0)
+let grid_h = Array.length grid
+
+let collect trails_from =
+  Array.foldi grid ~init:[] ~f:(fun y acc row ->
+      acc
+      @ Array.foldi row ~init:[] ~f:(fun x acc v ->
+            acc @ if Char.equal v '0' then trails_from (x, y) else []))
+
+let trails_from (x, y) =
+  let rec aux target (x, y) =
+    if x < 0 || x >= grid_w || y < 0 || y >= grid_h then []
+    else
+      let v = grid.(y).(x) in
+      if Char.equal v '.' then []
+      else if Char.equal v '9' then
+        if Char.equal target '9' then [ (x, y) ] else []
+      else if Char.equal v target then
+        neighbours (x, y)
+        |> List.fold ~init:[] ~f:(fun acc pos -> acc @ aux (next_target v) pos)
+      else []
+  in
+
+  aux '0' (x, y)
+
+let part1 () =
+  collect (fun (x, y) ->
+      trails_from (x, y) |> List.stable_dedup ~compare:Poly.compare)
+  |> List.length |> Utils.print_int
+
+let part2 () = collect trails_from |> List.length |> Utils.print_int
+
+let () =
+  part1 ();
+  part2 ()
